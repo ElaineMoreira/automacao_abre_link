841924da3174e001cfad156b035dabb271d1fb40
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-09-08 10:56:15 -0300

    altera script

---
+++ open_links.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+
+links = ["https://www.linkedin.com/in/elaine-moreira/", "https://github.com/ElaineMoreira"]
+
+for link in links:
+    webbrowser.open(link)
+    time.sleep(3)
+    
